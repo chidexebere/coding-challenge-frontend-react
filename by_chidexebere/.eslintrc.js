974e4499b54ca1c2595cd1255acbbb39b6085435
Chidi <chidex5@gmail.com>
2021-08-11 16:51:38 +0100

    Edit eslint config for prettier and add .eslintignore file

--- by_chidexebere/.eslintrc.js
+++ by_chidexebere/.eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    +'prettier/@typescript-eslint',
-    +'plugin:prettier/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
