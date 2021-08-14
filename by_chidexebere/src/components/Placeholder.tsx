7dcd873fea75e11f6e67224432e190caa96543a8
Chidi <chidex5@gmail.com>
2021-08-14 21:28:40 +0100

    Add placeholder component

---
+++ by_chidexebere/src/components/Placeholder.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Placeholder = (): JSX.Element => {
+  return (
+    <div className="placeholder">
+      <div className="placeholder__image"></div>
+      <p className="placeholder__name"></p>
+    </div>
+  );
+};
+
+export default Placeholder;
