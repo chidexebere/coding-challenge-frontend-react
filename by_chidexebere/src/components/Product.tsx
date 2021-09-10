b5497a647c4f50291234afa17675162ad13d769b
Chidi <chidex5@gmail.com>
2021-09-10 02:35:42 +0100

    use React memo to improve rendering performance of products

--- by_chidexebere/src/components/Product.tsx
+++ by_chidexebere/src/components/Product.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 interface ProductProps {
   productImageUrl: string;
   productName: string;
 }
 
-const Product = ({
-  productImageUrl,
-  productName,
-}: ProductProps): JSX.Element => {
-  return (
-    <div role="listitem" className="product">
-      <div className="product__image">
-        <img
-          src={productImageUrl}
-          loading="lazy"
-          width="120"
-          height="90"
-          alt={productName}
-        />
+const Product = memo(
+  ({ productImageUrl, productName }: ProductProps): JSX.Element => {
+    return (
+      <div role="listitem" className="product">
+        <div className="product__image">
+          <img
+            src={productImageUrl}
+            loading="lazy"
+            width="120"
+            height="90"
+            alt={productName}
+          />
+        </div>
+        <p className="product__name">{productName}</p>
       </div>
-      <p className="product__name">{productName}</p>
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default Product;
